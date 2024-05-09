82aaf5a40c393260776e724ce64864e7a1ca7858
jpineda <juan.cpinedam@gmail.com>
2024-05-09 14:12:19 -0500

    produced and recive by period

--- src/routes/dataDetailRoutes.js
+++ src/routes/dataDetailRoutes.js
@@ -2,9 +2,27 @@ import express from 'express'
 import controller from '../controllers/dataDetailController.js'
 const router = express.Router()
 
+/**
+* @swagger
+* /api/dashboard/list/{id}:
+*   get:
+*     summary: Obtiene el listado de datas que alimentan el dashboard - por cliente
+*     tags:
+*       - dashboard
+*     description: Retorna lista de data por cliente
+*     parameters:
+*       - in: path
+*         name: id
+*         description: ID de la data
+*         required: true
+*         schema:
+*           type: string
+*     responses:
+*       200:
+*         description: Operación exitosa. Devuelve los datos de un data
+*       500:
+*         description: Error interno del servidor.
+*/
+router.get('/list/:id',controller.listController)
 
-router.get('/list',controller.listController)
-router.get('/get',controller.getController)
-router.post('/insert',controller.insertController)
-router.post('/delete',controller.deleteController)
 export default router
