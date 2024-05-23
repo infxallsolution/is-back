ba7a6749634c09049dbaf06e29b11373cd763e95
jpineda <juan.cpinedam@gmail.com>
2024-05-23 14:45:44 -0500

    redirection https:

--- src/services/general/RedirectionService.js
+++ src/services/general/RedirectionService.js
@@ -5,7 +5,7 @@
 const redirectToModule = async (module) => {
   console.log(module) 
   try {
-    let url = 'https/app.infos.com/' + module
+    let url = 'https://app.infos.com/' + module
     return { redirectTo: url, status: 200 };
   } catch (err) {
     return { message: 'Error en el servidor', status: 500, error: err };
@@ -16,7 +16,7 @@ const redirectToModule = async (module) => {
 
 const redirectToDashboard = async () => {
   try {    
-    let url = 'https/is-front.infxsolution.com/'
+    let url = 'https://is-front.infxsolution.com/'
     return { redirectTo: url, status: 200 };
   } catch (err) {
     return { message: 'Error en el servidor', status: 500, error: err };
